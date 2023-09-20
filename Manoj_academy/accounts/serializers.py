d88732866e1324527e058733885b5141bfb59307
M-an-o-j <ronaldomano7070@gmail.com>
2023-09-20 17:09:36 +0530

    User functionality

---
+++ Manoj_academy/accounts/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['__all__']
+    
+
+class Registerserializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    
+    def validate(self, data):
+
+        if User.objects.filter(username= data['username']).exists():
+            raise serializers.ValidationError('Username is already taken')
+        
+        return data
+        
+    def create(self, validate_data):
+        user = User.objects.create(
+            first_name = validate_data['first_name'],
+            last_name = validate_data['last_name'],
+            username = validate_data['username'],
+            email = validate_data['email'],
+        )
+        user.set_password(validate_data['password'])
+        user.save()
+        return validate_data
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+
+        if not User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError('User not found')
+
+        return data
+    
+    # def get_jwt_token(self, data):
+    #     user = authenticate(username = data['username'], password= data['password'])
+        
+    #     print(user)
+
+    #     if not user:
+    #         return {'message':'invalid credentials', 'data':{}}
+        
+    #     refresh = RefreshToken.for_user(user)
+
+    #     return {'message':'logged in', 'data':{'token':{ 'refresh': str(refresh), 'access':str(refresh.access_token) }}}
+
+
+
---
+++ Manoj_academy/accounts/views.py
@@ -0,0 +1,108 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .serializers import Registerserializer, LoginSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = Registerserializer(data=data)
+
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,
+                    'message': 'something went wrong'
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
+
+            return Response({
+                'data': serializer.data,
+                'message': 'user created successfully'
+            }, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            print(e)
+            return Response({
+                'data': {},
+                'message': 'something went wrong in try'
+            })
+
+
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = LoginSerializer(data=data)
+
+            if not serializer.is_valid():
+                return Response({
+                    'status': False,
+                    'message': serializer.errors
+                }, status.HTTP_400_BAD_REQUEST)
+
+            user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+            
+            tok = None
+
+            try:
+                tok = Token.objects.get(user = user)
+                print(tok)
+            except Exception as e:
+                print(e)
+
+            if tok== None:
+                    if user is not None:
+                        response_data = {
+                            'message': 'Login successful',
+                            'user_id': user.id,
+                            'username': user.username,
+                            'email': user.email,
+                            'first_name': user.first_name,
+                            'last_name': user.last_name
+                        }
+                    else:
+                        return Response({
+                            'message': 'Invalid credentials'
+                        }, status=status.HTTP_401_UNAUTHORIZED)
+
+                    token, __ = Token.objects.get_or_create(user=user)
+                    Response
+                    response = Response({
+                        'status': True,
+                        'message': 'User Logged in',
+                        'Token': str(token),
+                        'data': response_data
+                    }, status.HTTP_201_CREATED)
+                    response.set_cookie('MA_token',token)
+                    return response
+            else:
+                return Response({
+                        'message':'Simultaneous login feature is not available'
+                    })
+        except Exception as e:
+            print(e, "hii")
+            return Response({
+                'data': {},
+                'message': 'something went wrong in try'
+            })
+            
+
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.auth.delete()
+        response = Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
+        response.delete_cookie('MA_token')
+        return response
